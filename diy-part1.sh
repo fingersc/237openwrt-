1e2c1ad7883b6f5f28facdc70a01c887fa650220
fingersc <156872602+fingersc@users.noreply.github.com>
2024-01-17 22:33:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,4 +21,4 @@ git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic
 git clone https://github.com/sbwml/luci-app-alist package/alist
 git clone https://github.com/kongfl888/luci-app-timedreboot
 git clone https://github.com/pymumu/luci-app-smartdns
-git clone https://github.com/destan19/OpenAppFilter/tree/master
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
