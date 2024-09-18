950bc0b39fed39b2663da8e6ff4b2660af4b7940
fingersc <156872602+fingersc@users.noreply.github.com>
2024-09-18 23:49:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,7 +8,8 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+#添加额外软件包
+#git clone https://github.com/pymumu/openwrt-smartdns package/xxx
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
