d24b93227d14fa89e997e2a9644d25d7c7a56fcc
fingersc <156872602+fingersc@users.noreply.github.com>
2024-09-22 20:14:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,8 +8,12 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
+#删除部分原软件包，以保证可以后续添加的软件包可以被编译
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/applications/luci-app-smartdns
 #添加额外软件包
-#git clone https://github.com/pymumu/openwrt-smartdns package/xxx
+git clone https://github.com/immortalwrt/packages/tree/master/net/smartdns package/smartdns
+git clone https://github.com/immortalwrt/luci/tree/master/applications/luci-app-smartdns package/luci-app-smartdns
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
