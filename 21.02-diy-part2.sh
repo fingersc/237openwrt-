018ee6eb79c9f4476747af8bd4d3a270c2eaeebd
fingersc <156872602+fingersc@users.noreply.github.com>
2024-10-08 22:02:21 +0800

    Update 21.02-diy-part2.sh

--- 21.02-diy-part2.sh
+++ 21.02-diy-part2.sh
@@ -10,12 +10,12 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
 # 删除部分原软件包，以保证可以后续添加的软件包可以被编译
-# rm -rf feeds/packages/net/smartdns
-# rm -rf feeds/luci/applications/luci-app-smartdns
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/applications/luci-app-smartdns
 
 # 添加额外软件包
-# git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns/
-# git clone https://github.com/pymumu/openwrt-smartdns.git feeds/luci/applications/luci-app-smartdns/
+git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns/
+git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns/
 # git clone https://github.com/pymumu/openwrt-smartdns.git package/smartdns
 # git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git lucky-repo
@@ -26,4 +26,4 @@ cp -rf lucky-repo/lucky package/lucky
 # sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # 貌似需要重新执行以下代码才能正确进行编译
-# ./scripts/feeds install -a -f
+./scripts/feeds install -a
